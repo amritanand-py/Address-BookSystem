a4f560e992d4323291ba7e5ec64589e7b01e342b
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-01-23 16:06:31 +0530

    [Amrit Anand] Add: Base File main UC statement iniated

---
+++ AddressBookSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressBooksystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookMain obj1 = new AddressBookMain();
+            obj1.welcome();
+        }
+    }
+
+    class AddressBookMain
+    {
+        public void welcome()
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+
+    }
+
+
+}
